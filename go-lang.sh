be68bb125e2ee8681e271d0346d560308a54aa86
Todd Sampson <tsampson@cloudspace.com>
2014-07-10 11:32:10 -0400

    Setup go to use shared nfs folder instead of building local workspace.

--- go-lang.sh
+++ go-lang.sh
@@ -4,11 +4,13 @@
 sudo apt-get install -y golang
 
 # Setup Go Path
-echo -e "export GOPATH=/home/vagrant/go\nexport PATH=$PATH:/home/vagrant/go/bin" | tee ~/.bash_profile ~/.bashrc
+echo -e "export GOPATH=/srv/go\nexport PATH=$PATH:/srv/go/bin" | tee ~/.bash_profile ~/.bashrc
 . ~/.bash_profile
 
-# Create workspace directories
-mkdir -p $GOPATH/bin
-mkdir -p $GOPATH/pkg
-mkdir -p $GOPATH/src
-mkdir -p $GOPATH/src/github.com
+# Uncomment below to create local workspace directories
+# I prefer to launch the devops box with vagrant at /srv/devops
+# and mount a separate go directory structure at /srv/go
+# mkdir -p $GOPATH/bin
+# mkdir -p $GOPATH/pkg
+# mkdir -p $GOPATH/src
+# mkdir -p $GOPATH/src/github.com
