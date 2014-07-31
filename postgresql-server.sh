aedef9cb716029e3e20db1e16911b0aaf99a2542
Matthew Cupples <mcupples@cloudspace.com>
2014-07-31 17:22:18 -0400

    fixed the local logins

--- postgresql-server.sh
+++ postgresql-server.sh
@@ -7,4 +7,10 @@ sudo apt-get install -y postgresql-9.3 postgresql-contrib-9.3 postgresql-client-
 sudo -u postgres psql postgres postgres -c "ALTER USER postgres WITH PASSWORD 'postgres'"
 
 # allow all users to access postgres locally from any user
+sudo rm /etc/postgresql/9.3/main/pg_hba.conf 
+echo "local   all             postgres                                peer"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
 echo "local   all             all                                     md5"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
+echo "local   all             all                                     peer"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
+echo "host    all             all             127.0.0.1/32            md5"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
+echo "host    all             all             ::1/128                 md5"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
+sudo service postgresql restart
