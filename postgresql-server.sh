64e066888020ae24d4968d14df2ce649d1aeca6f
Matthew Cupples <mcupples@cloudspace.com>
2014-07-31 17:37:40 -0400

    made it so you can use postgres user

--- postgresql-server.sh
+++ postgresql-server.sh
@@ -4,12 +4,12 @@
 sudo apt-get install -y postgresql-9.3 postgresql-contrib-9.3 postgresql-client-9.3
 
 # Set password for postgres database user
-sudo -u postgres psql postgres postgres -c "ALTER USER postgres WITH PASSWORD 'postgres'"
+sudo -u postgres psql postgres postgres -c "ALTER USER postgres WITH ENCRYTPED PASSWORD 'postgres'"
 
 # allow all users to access postgres locally from any user
 sudo rm /etc/postgresql/9.3/main/pg_hba.conf 
-echo "local   all             postgres                                peer"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
 echo "local   all             all                                     md5"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
+echo "local   all             postgres                                peer"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
 echo "local   all             all                                     peer"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
 echo "host    all             all             127.0.0.1/32            md5"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
 echo "host    all             all             ::1/128                 md5"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
