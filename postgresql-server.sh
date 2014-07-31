b4613f37c24af77567cfbb8757627d4e2202b19c
Matthew Cupples <mcupples@cloudspace.com>
2014-07-31 16:39:55 -0400

    append w/ sudo

--- postgresql-server.sh
+++ postgresql-server.sh
@@ -7,4 +7,4 @@ sudo apt-get install -y postgresql-9.3 postgresql-contrib-9.3 postgresql-client-
 sudo -u postgres psql postgres postgres -c "ALTER USER postgres WITH PASSWORD 'postgres'"
 
 # allow all users to access postgres locally from any user
-echo "local   all             all                                     md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+echo "local   all             all                                     md5"|sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
