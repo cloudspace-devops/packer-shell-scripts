e58a92ddbd28eb379e55786e95a90e7f1737930c
Matthew Cupples <mcupples@cloudspace.com>
2014-09-25 12:31:55 -0400

    added screen

--- base.sh
+++ base.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install -y build-essential
-sudo apt-get install -y linux-headers-$(uname -r) dkms nfs-common git-core whois traceroute htop unzip sysstat vim libmysqlclient-dev postgresql-server-dev-9.3 wget
+sudo apt-get install -y linux-headers-$(uname -r) dkms nfs-common git-core whois traceroute htop unzip sysstat vim libmysqlclient-dev postgresql-server-dev-9.3 wget screen
 sudo apt-mark hold grub grub-common grub-pc grub-pc-bin grub2-common grub-legacy-ec2
 sudo apt-get -y upgrade
 
