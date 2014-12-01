1ab6793e8b46eaa882f13c86194ac04d8e9fb41d
Todd Sampson <tsampson@cloudspace.com>
2014-12-01 17:29:55 -0500

    Removing catkin stuff that is causing the ROS build to fail.

--- ros-indigo.sh
+++ ros-indigo.sh
@@ -13,19 +13,20 @@ sudo apt-get install -y ros-indigo-desktop-full
 # Initialize rosdep
 sudo rosdep init
 rosdep update
-sudo chmod 777 /opt/ros/indigo/setup.bash
 
 # Setup environment
 echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
+# export | grep ROS
+
 # Setup rosinstall
 sudo apt-get install -y python-rosinstall
 
 # Setup ROS Workspace
-mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/src
-catkin_init_workspace
-cd ~/catkin_ws/
-catkin_make
-source devel/setup.bash
+# mkdir -p ~/catkin_ws/src
+# cd ~/catkin_ws/src
+# catkin_init_workspace
+# cd ~/catkin_ws/
+# catkin_make
+# source devel/setup.bash
