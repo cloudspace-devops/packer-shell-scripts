048b4938637b6f469ec3bdf699e6f79cbd3a70f5
Rusty Phillips <rusty@cloudspace.com>
2015-05-22 11:38:16 -0400

    Add modern golang.

--- go-lang.sh
+++ go-lang.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 # Install Go
-sudo apt-get install -y golang
+#sudo apt-get install -y golang
 
 # Install Mercurial, Subversion, Git, Bzr, and CVS Version Control Systems
 sudo apt-get install -y mercurial
@@ -10,9 +10,15 @@ sudo apt-get install -y git
 sudo apt-get install -y bzr
 sudo apt-get install -y cvs
 
+curl -O https://storage.googleapis.com/golang/go1.4.1.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.4.1.linux-amd64.tar.gz
+rm go1.4.1.linux-amd64.tar.gz
+
 # Setup Go Path
-echo -e "export GOPATH=/srv/go\nexport PATH=$PATH:/srv/go/bin" | tee ~/.bash_profile ~/.bashrc
+echo -e "export GOPATH=/srv/go\nexport PATH=$PATH:/srv/go/bin:/usr/local/go/bin" | tee ~/.bash_profile ~/.bashrc
 . ~/.bash_profile
+cd /srv/go
+go get github.com/mattn/gom
 
 # Uncomment below to create local workspace directories
 # I prefer to launch the devops box with vagrant at /srv/devops
