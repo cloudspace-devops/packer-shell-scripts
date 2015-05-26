4f390e1ef42c8b61defc0d57aadd38c878003333
Rusty Phillips <rusty@cloudspace.com>
2015-05-26 16:26:07 -0400

    Fixed issues with file.

--- go-lang.sh
+++ go-lang.sh
@@ -11,18 +11,17 @@ sudo apt-get install -y bzr
 sudo apt-get install -y cvs
 
 curl -O https://storage.googleapis.com/golang/go1.4.1.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.4.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.4.1.linux-amd64.tar.gz
 rm go1.4.1.linux-amd64.tar.gz
 
 # Setup Go Path
 echo -e "export GOPATH=/srv/go\nexport PATH=$PATH:/srv/go/bin:/usr/local/go/bin" | tee ~/.bash_profile ~/.bashrc
 . ~/.bash_profile
-cd /srv/go
-go get github.com/mattn/gom
 
 # Uncomment below to create local workspace directories
 # I prefer to launch the devops box with vagrant at /srv/devops
 # and mount a separate go directory structure at /srv/go
+# go get github.com/mattn/gom
 # mkdir -p $GOPATH/bin
 # mkdir -p $GOPATH/pkg
 # mkdir -p $GOPATH/src
